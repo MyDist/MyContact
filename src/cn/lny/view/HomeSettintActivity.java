a678743f873af3381380a2861ef6883dd7f9b59b
lny <linanyang6688@163.com>
2016-05-08 17:37:47 +0800

    Dist

--- src/cn/lny/view/HomeSettintActivity.java
+++ src/cn/lny/view/HomeSettintActivity.java
@@ -6,25 +6,9 @@ import android.os.Bundle;
 
 public class HomeSettintActivity extends Activity {
 	
-	
-	
-	
-	
-	
-	
-	
-	
 	public void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_setting_page);
 		
 	}
-	
-	
-	
-	
-	
-	
-	
 }
